a387aec963445ff46ff097d1698b86750ad0bc82
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-04 16:27:18 +0000

    add var for getting current timestamp

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const OPEN_WEATHER_MAP_API_KEY = "691ac0267ff69aea88b780e2223ae9f4";
+
+// var formHeading = $("#form-heading");
+// var searchForm = $("search-form");
+// var searchInput = $("search-input");
+// var searchBtn = $("search-button");
+// var searchBtn = $("search-button");
+// var historyDiv = $("history");
+// var historyDiv = $("history");
+// var todaySection = $("today");
+// var forecastSection = $("forecast");
+
+const currentDate = dayjs();
+
+// function getFiveDayWeatherForecast(params) {}
