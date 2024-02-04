b504572bfcaaf27f4922076f479b1cbd24096d4d
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-04 16:26:32 +0000

    create structure for web project

---
+++ scripts/utils/apiKeys.js
@@ -0,0 +1,1 @@
+// const OPEN_WEATHER_MAP_API_KEY = "691ac0267ff69aea88b780e2223ae9f4";
---
+++ scripts/utils/elements.js
@@ -0,0 +1,9 @@
+// var formHeading = $("#form-heading");
+// var searchForm = $("search-form");
+// var searchInput = $("search-input");
+// var searchBtn = $("search-button");
+// var searchBtn = $("search-button");
+// var historyDiv = $("history");
+// var historyDiv = $("history");
+// var todaySection = $("today");
+// var forecastSection = $("forecast");
